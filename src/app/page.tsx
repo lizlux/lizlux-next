6f84a5282e0e5e9b0dfb72413664cf3c88bdbb22
lizlux <liz@lizlux.net>
2025-03-11 09:50:04 -0700

    replacing home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,93 +3,161 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="container">
+      <Image
+        aria-hidden
+        src="/Vine.svg"
+        alt="decorative vine"
+        width={900}
+        height={1211}
+        className="vine"
+      />
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <header className="header">
+        <span className="headshot">
+          <Image
+            aria-hidden
+            src="/Liz_Lee_Headshot.jpg"
+            alt="Liz Lee headshot"
+            width={160}
+            height={240}
+          />
+        </span>
+        <h1>Liz Lee</h1>
+        <div className="details">
+          <p>Lead Front End Engineer</p>
+          <p>Engineering Mangement</p>
+          <p>JavaScript / Typescript</p>
+          <p>HTML / CSS</p>
+          <p>React / Redux</p>
+          <p>Angular / NgRx</p>
+          <p>UI Design Systems</p>
+          <p>Expert Level</p>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </header>
+      <section className="content">
+        <h2>
+          Lets Build
+          <br />
+          Great Products
+        </h2>
+        <nav className="nav">
+          <ul>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/lizluxlee/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                LinkedIn
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/lizlux"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="/Liz_Lee_Resume.pdf" target="_blank" rel="noreferrer">
+                Resume
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </section>
     </div>
+
+    // <div className={styles.page}>
+    //   <main className={styles.main}>
+    //     <Image
+    //       className={styles.logo}
+    //       src="/next.svg"
+    //       alt="Next.js logo"
+    //       width={180}
+    //       height={38}
+    //       priority
+    //     />
+    //     <ol>
+    //       <li>
+    //         Get started by editing <code>src/app/page.tsx</code>.
+    //       </li>
+    //       <li>Save and see your changes instantly.</li>
+    //     </ol>
+
+    //     <div className={styles.ctas}>
+    //       <a
+    //         className={styles.primary}
+    //         href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         <Image
+    //           className={styles.logo}
+    //           src="/vercel.svg"
+    //           alt="Vercel logomark"
+    //           width={20}
+    //           height={20}
+    //         />
+    //         Deploy now
+    //       </a>
+    //       <a
+    //         href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //         className={styles.secondary}
+    //       >
+    //         Read our docs
+    //       </a>
+    //     </div>
+    //   </main>
+    //   <footer className={styles.footer}>
+    //     <a
+    //       href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       <Image
+    //         aria-hidden
+    //         src="/file.svg"
+    //         alt="File icon"
+    //         width={16}
+    //         height={16}
+    //       />
+    //       Learn
+    //     </a>
+    //     <a
+    //       href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       <Image
+    //         aria-hidden
+    //         src="/window.svg"
+    //         alt="Window icon"
+    //         width={16}
+    //         height={16}
+    //       />
+    //       Examples
+    //     </a>
+    //     <a
+    //       href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       <Image
+    //         aria-hidden
+    //         src="/globe.svg"
+    //         alt="Globe icon"
+    //         width={16}
+    //         height={16}
+    //       />
+    //       Go to nextjs.org →
+    //     </a>
+    //   </footer>
+    // </div>
   );
 }
